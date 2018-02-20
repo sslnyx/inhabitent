1d5ad401e81095ac82596dc03de2083a902f401c
sslnyx <sslnyx@gmail.com>
2018-02-20 11:31:20 -0800

    added post function

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -12,8 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<!-- <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"> -->
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/style.css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/build/css/style.min.css" />
 
 
 	<?php wp_head(); ?>
