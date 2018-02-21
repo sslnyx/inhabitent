c3164a1625b20759a0b5e1aad4df053ac6d33e07
sslnyx <sslnyx@gmail.com>
2018-02-21 15:55:09 -0800

    added product-type-logo

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -25,7 +25,7 @@ get_header(); ?>
                         <?php foreach ( $terms as $term ) :
                             ?>
                             <div class="product-type-block-wrapper">
-                                <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg"
+                                <img src="<?php echo get_template_directory_uri() . '/img/product-type-logo/' . $term->slug; ?>.svg"
                                      alt="<?php echo $term->name; ?>"/>
                                 <p><?php echo $term->description; ?></p>
                                 <p>
